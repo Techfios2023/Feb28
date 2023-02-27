319cb307a292e45b7a3b16e38c9995a3e5603ea4
Techfios2023 <122751955+Techfios2023@users.noreply.github.com>
2023-02-27 17:52:02 -0600

    Add files via upload
    XML

---
+++ XML_Project_Sep2022/src/test/java/test/LoginTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import page.DashboardPage;
+import page.LoginPage;
+import util.BrowserFactory;
+
+
+public class LoginTest {
+	
+	WebDriver driver;
+	
+	
+	
+	String userName;
+	String password;
+	
+	@Test
+	@Parameters({"UserName", "Password"})
+	public void validUserShouldBeAbleToLogin(String userName, String password) {
+
+		driver = BrowserFactory.init();
+		
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		loginPage.enterUserName(userName);
+		loginPage.insertPassword(password);
+		loginPage.clickOnSininButton();
+		
+		DashboardPage dashboardPage = PageFactory.initElements(driver, DashboardPage.class);
+		dashboardPage.validateDashboardPage();
+		
+		BrowserFactory.tearDown();
+		
+	}
+	
+
+}
